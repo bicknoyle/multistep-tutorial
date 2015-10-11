af3c86cdb00e1c377dc3d658d34868e601fd8b4f
vagrant <nsboyle@gmail.com>
2015-10-11 23:12:30 +0000

    Using built-in casts and some tabs to spaces clean-up

--- app/Question.php
+++ app/Question.php
@@ -6,15 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-	public function getValuesAttribute($value) {
-		return json_decode($value, true);
-	}
-
-    public function setValuesAttribute($value) {
-    	if (!is_array($value)) {
-    		$value = [$value];
-    	}
-
-    	 $this->attributes['values'] = json_encode($value);
-    }
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'values' => 'array',
+    ];
 }
--- resources/views/survey/step.blade.php
+++ resources/views/survey/step.blade.php
@@ -4,16 +4,16 @@
 
 {!! Form::model($survey) !!}
     @foreach($questions as $question)
-    	@if('radio' == $question->type)
-    		{{ $question->text }}<br>
-    		@foreach($question->values as $value)
-    			<label>{!! Form::radio($question->name, $value) !!} {{ $value }}</label><br>
-    		@endforeach
-    	@else
-    		{!! Form::label($question->name, $question->text) !!}<br>
-    		{!! Form::text($question->name) !!}<br>
-    	@endif
-    	<br>
+        @if('radio' == $question->type)
+            {{ $question->text }}<br>
+            @foreach($question->values as $value)
+                <label>{!! Form::radio($question->name, $value) !!} {{ $value }}</label><br>
+            @endforeach
+        @else
+            {!! Form::label($question->name, $question->text) !!}<br>
+            {!! Form::text($question->name) !!}<br>
+        @endif
+        <br>
     @endforeach
     <button type="submit">Submit</button>
 {!! Form::close() !!}
